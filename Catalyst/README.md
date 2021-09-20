ee3784bc961b3a39f1b343bb37fd670502b30e22
William <Williamqzy0705@gmail.com>
2021-09-20 21:49:04 +1000

    partial finish(read CSV, create table, parse CLI)

---
+++ Catalyst/README.md
@@ -0,0 +1,14 @@
+# Programming Evaluation For Catalyst IT Australia Pty Limited
+user_upload.py
+
+- [ ] PostgreSQL
+  - [ ] Connect
+  - [ ] Create table 'users'
+  - [ ] Insert
+- [ ] Capitalise Names
+  - [ ] Special Cases
+    - [ ] o'reilly -> O'Reilly
+    - [ ] 
+- [ ] Valid emails should be lower cases
+- [ ] Command line directives
+- [ ] Error message on STDOUT 
---
+++ Catalyst/user_upload.py
@@ -0,0 +1,64 @@
+# -*- coding:UTF-8 -*-
+
+import psycopg2
+import sys
+import pandas as pd
+import string, regex
+
+# Read CSV & Capitalise Names
+filename = "users.csv"
+df = pd.read_csv(
+    filename,
+    index_col=None,
+    # header=None
+)
+df.columns = ["name", "surname", "email"]
+
+for index, row in df.iterrows():
+    print(df.name[index].title(), df.surname[index].title(), df.email[index].lower())
+# save df into a Dict
+# email validation
+
+# Parse cli arguments
+argList = []
+for i in range(len(sys.argv) - 1):
+    argList.append(sys.argv[i + 1])
+# print(f"Arguments of the script : {sys.argv[1:]=}")
+# print(f"Arguments of the script : "+str(argList))
+
+for arg in argList:
+    if "--file" in arg:
+        input_csv = arg.split("=")[-1]
+
+    if "-u=" in arg:
+        pg_username = arg.split("=")[-1]
+
+    if "-p=" in arg:
+        pg_userpassword = arg.split("=")[-1]
+
+    if "-h=" in arg:
+        pg_host = arg.split("=")[-1]
+
+    if "create_table" in arg:
+        create_table()  # Todo
+
+# Connect to Postgres
+pg_connect = psycopg2.connect(
+    database="postgres",
+    user=pg_username,
+    password=pg_userpassword,
+    host=pg_host.split(":")[0],
+    port=pg_host.split(":")[1],
+)
+# create a cursor object
+cursor = pg_connect.cursor()
+cursor.execute(
+    "CREATE TABLE users (surname varchar, surname varchar, email varchar PRIMARY KEY);"
+)
+
+# cursor.execute("select version()")
+# use fetchone()to fetch single line
+data = cursor.fetchone()
+print("pg_Connectection established to: ", data)
+# Close connection
+pg_connect.close()
